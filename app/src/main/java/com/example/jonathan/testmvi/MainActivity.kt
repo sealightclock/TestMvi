9829707a77e78d6e6d63bc9e2e0bc98b9162a785
Sealight Clock <chbebram@hotmail.com>
2025-05-14 16:09:21 -0700

    MainActivity: use "by ViewModels()" delegate to create UserViewModel for lifecycle awareness, dependency injection, and saved state handling.

--- app/src/main/java/com/example/jonathan/testmvi/MainActivity.kt
+++ app/src/main/java/com/example/jonathan/testmvi/MainActivity.kt
@@ -3,15 +3,15 @@ package com.example.jonathan.testmvi
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.viewModels
 import com.example.jonathan.testmvi.features.user.presentation.view.UserScreen
 import com.example.jonathan.testmvi.features.user.presentation.viewmodel.UserViewModel
 
 class MainActivity : ComponentActivity() {
+    private val viewModel: UserViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        // Create ViewModel:
-        val viewModel = UserViewModel()
 
         setContent {
             // Connect to the View (Jetpack Compose Screen):
